c3638e1537ead35545a51fddb0b55ed58a3522f9
Mohamed Abdulla <Mohamed.A@impacteers.com>
2026-07-06 18:47:55 +0530

    tech stack updated in about section

--- src/components/Frameworks.jsx
+++ src/components/Frameworks.jsx
@@ -2,33 +2,40 @@ import { OrbitingCircles } from "./OrbitingCircles";
 
 export function Frameworks() {
   const skills = [
-    "auth0",
-    "blazor",
-    "cplusplus",
-    "csharp",
-    "css3",
-    "dotnet",
-    "dotnetcore",
-    "git",
-    "html5",
-    "javascript",
-    "microsoft",
-    "react",
-    "sqlite",
-    "tailwindcss",
-    "vitejs",
-    "wordpress",
+    "Next.js.png",
+    // "auth0",
+    "javascript.svg",
+    // "blazor",
+    "vitejs.svg",
+    // "cplusplus",
+    "git.svg",
+    // "csharp",
+    "css3.svg",
+    // "dotnet",
+    "Java.png",
+    // "dotnetcore",
+
+    "html5.svg",
+
+    // "microsoft",
+    "react.svg",
+    // "sqlite",
+    "tailwindcss.svg",
+
+    // "wordpress",
+
+    "TypeScript.png",
   ];
   return (
     <div className="relative flex h-[15rem] w-full flex-col items-center justify-center">
       <OrbitingCircles iconSize={40}>
         {skills.map((skill, index) => (
-          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+          <Icon key={index} src={`assets/logos/${skill}`} />
         ))}
       </OrbitingCircles>
       <OrbitingCircles iconSize={25} radius={100} reverse speed={2}>
         {skills.reverse().map((skill, index) => (
-          <Icon key={index} src={`assets/logos/${skill}.svg`} />
+          <Icon key={index} src={`assets/logos/${skill}`} />
         ))}
       </OrbitingCircles>
     </div>
